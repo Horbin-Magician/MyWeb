858331ba1e44f662df056fb2c4bdbdf1eb94175e
Horbin <2240045751@qq.com>
2021-05-19 04:05:18 -0700

    beian

--- Client/src/components/footer/footer.jsx
+++ Client/src/components/footer/footer.jsx
@@ -1,17 +1,27 @@
-import {Component} from 'react'
+import { Component } from 'react'
+import { Row, Col} from 'antd';
 
+import beianIcon from '../../assets/imgs/beianIcon.png';
 import './footer.less'
 /*
 首页的路由组件
-*/ 
+*/
 export default class Footer extends Component {
-    render() {
-      return (
-        <div className='footer'>
-          <a  href='https://beian.miit.gov.cn'>赣ICP备2021004310号-1</a>
-          &nbsp;&nbsp;&nbsp;
+  render() {
+    return (
+      <Row className='footer' justify="center" gutter={{ xs: 8, sm: 16, md: 12}}>
+        <Col>
+          <img src={beianIcon} alt='' />
+          <a target="_blank" rel="noreferrer" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42011102004461">
+          鄂公网安备 42011102004461号</a>
+        </Col>
+        <Col>
+          <a href='https://beian.miit.gov.cn'>赣ICP备2021004310号-1</a>
+        </Col>
+        <Col>
           ©2021 Horbin
-        </div>
-       );
-    }
+        </Col>
+      </Row>
+    );
+  }
 }
