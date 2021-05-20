1b4d50c7252ed496a3870bd15f642b89485c17ec
Horbin <2240045751@qq.com>
2021-05-20 08:35:28 -0700

    update home

--- Client/src/pages/home/home.jsx
+++ Client/src/pages/home/home.jsx
@@ -1,14 +1,24 @@
 import { Component } from 'react'
+import { Input, Row, Col } from 'antd';
 
 /*
 首页的路由组件
 */
+const { Search } = Input;
+
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                这是首页，但还没有开发哦～
-            </div>
-        );
-    }
+  onSearch = value => {
+    window.open('https://www.baidu.com/s?&wd='+value)
+  }
+  render() {
+    return (
+      <div style={{paddingTop:'300px'}}>
+        <Row justify="center">
+          <Col xs={{ span: 22 }} sm={{ span: 22 }} md={{ span: 12 }} lg={{ span: 8 }}>
+            <Search style={{textAlign:'center'}} placeholder="请填写搜索内容" onSearch={this.onSearch} enterButton />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
