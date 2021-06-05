99f0dae7d9d458715cd962b61aeddf9783432134
Horbin <2240045751@qq.com>
2021-06-05 01:19:09 -0700

    update about

--- Client/src/pages/about/about.jsx
+++ Client/src/pages/about/about.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import ReactMarkdown from 'react-markdown';
 import { Tabs,Col,Row } from 'antd';
+import { CoffeeOutlined } from '@ant-design/icons'
 
 import aboutArt from '../../assets/passages/aboutArt.md'
 import aboutTec from '../../assets/passages/aboutTec.md'
@@ -13,14 +14,14 @@ export default class About extends Component {
   render() {
     return (
       <Tabs defaultActiveKey="art" centered className='about-content'>
-        <TabPane tab="文艺范" key="art" className='about-content-art'>
+        <TabPane tab={<CoffeeOutlined style={{ fontSize: '24px', margin:'0'}}/>} key="art" className='about-content-art'>
           <Row justify='center'>
             <Col xs={{ span: 22 }} sm={{ span: 20 }} md={{ span: 16 }} lg={{ span: 12 }}>
               <ReactMarkdown children={aboutArt}/>
             </Col>
           </Row>
         </TabPane>
-        <TabPane tab="技术范" key="tec" className='about-content-tec'>
+        <TabPane tab={<CoffeeOutlined rotate={180} style={{ fontSize: '24px', margin:'0'}}/>} key="tec" className='about-content-tec'>
           <Row justify='center'>
             <Col xs={{ span: 22 }} sm={{ span: 20 }} md={{ span: 16 }} lg={{ span: 12 }}>
               <ReactMarkdown children={aboutTec}/>
