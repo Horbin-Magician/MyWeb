e3305a7c35636521b1108fed6cdd51b79b132f17
Horbin <2240045751@qq.com>
2021-07-06 18:27:28 -0700

    fix search bug

--- Server/tools/spiders/baiduSpider.py
+++ Server/tools/spiders/baiduSpider.py
@@ -1,15 +1,15 @@
+from datetime import time
 import requests
 from bs4 import BeautifulSoup
 
 
 headers = {
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
   "Referer": "https://www.baidu.com",
-  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
   "Accept-Encoding": "gzip, deflate, br",
-  # "is_referer" : "https://www.baidu.com/",
-  # "Host" : "www.baidu.com",
-  # "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+  "Host" : "www.baidu.com",
+  "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
 }
 
 def get_data(keyword):
@@ -19,14 +19,23 @@ def get_data(keyword):
 
 def crawl_html(url):
   html = None
-  try:
-    html = requests.get(url, headers=headers, timeout=5)
-    if(html.status_code != 200):
+  ifGetHtml = False
+
+  while(ifGetHtml == False):
+    try:
+      html = requests.get(url, headers=headers, timeout=5)
+      if(html.status_code != 200):
+        return {'status':1, 'errorMessage':'出现异常！'}
+    except requests.exceptions.RequestException as e:
       return {'status':1, 'errorMessage':'出现异常！'}
-  except requests.exceptions.RequestException as e:
-    return {'status':1, 'errorMessage':'出现异常！'}
-  
-  soup = BeautifulSoup(html.text, 'lxml')
+    
+    soup = BeautifulSoup(html.text, 'lxml')
+
+    timeout = soup.select('.timeout')
+    print(len(timeout))
+    if(len(timeout) == 0):
+      ifGetHtml = True
+
   moreUrl = ''
   datas = []
 
